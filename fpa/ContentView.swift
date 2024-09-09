ed904291b8451331dcb5ba4883ed001d2239daaa
psbagga17 <psbagga17@gmail.com>
2024-09-09 12:34:13 -0400

    Initial Commit

---
+++ fpa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fpa
+//
+//  Created by Puneet Singh Bagga on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fpa/fpaApp.swift
@@ -0,0 +1,17 @@
+//
+//  fpaApp.swift
+//  fpa
+//
+//  Created by Puneet Singh Bagga on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct fpaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
